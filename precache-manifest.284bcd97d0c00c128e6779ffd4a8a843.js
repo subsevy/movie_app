b7823ca692237fe2c198336e381be2e61eb6714e
subsevy <sub0713@gmail.com>
2019-11-18 20:00:26 +0900

    Updates

---
+++ precache-manifest.284bcd97d0c00c128e6779ffd4a8a843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb64ecbd5070e1dad15abbabefd65e66",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "beae90caca411b287497",
+    "url": "/movie_app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "6f0599dff9eecd2a0e73",
+    "url": "/movie_app/static/js/2.43e6c5f9.chunk.js"
+  },
+  {
+    "revision": "beae90caca411b287497",
+    "url": "/movie_app/static/js/main.c57eb41b.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
